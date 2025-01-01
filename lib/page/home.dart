6bc169cb4529d777a326008e82288be90f5a97c4
Archie Saskara <saskaraarchie@gmail.com>
2025-01-01 14:45:55 +0800

    added some potentially efficient fix

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,19 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:fitopia/page/premium.dart';
 import 'package:fitopia/page/profile.dart';
+import 'package:fitopia/page/recipe.dart';
 import 'package:fitopia/page/settings.dart';
 import 'package:fitopia/page/workout/core.dart';
 import 'package:fitopia/page/workout/lowerBody.dart';
-import 'package:fitopia/widget/floatingNavBar.dart';
-import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:fitopia/page/workout/upperBody.dart';
+import 'package:fitopia/widget/floatingNavBar.dart';
+import 'package:fitopia/widget/weeklyChallenge.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   void _launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
     } else {
       throw 'Could not launch $url';
     }
@@ -21,12 +25,16 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Get the currently signed-in user
+    final user = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 16.0),
         child: Column(
           children: [
+            // User Greeting Section
             Container(
               padding: const EdgeInsets.all(16.0),
               color: Colors.white,
@@ -39,14 +47,14 @@ class HomePage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'Hi, User',
+                          'Hi, ${user?.displayName ?? "User"}', // Use FirebaseAuth's displayName
                           style: TextStyle(
                             fontSize: 24,
                             fontWeight: FontWeight.bold,
                             color: Colors.green[800],
                           ),
                         ),
-                        Text(
+                        const Text(
                           "It's time to challenge your limits.",
                           style: TextStyle(fontSize: 16, color: Colors.black87),
                         ),
@@ -54,35 +62,30 @@ class HomePage extends StatelessWidget {
                     ),
                   ),
                   IconButton(
-                    icon: Icon(Icons.account_circle, size: 50, color: Colors.black87),
+                    icon: const Icon(Icons.account_circle, size: 50, color: Colors.black87),
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => ProfilePage()),
+                        MaterialPageRoute(builder: (context) => const ProfilePage()),
                       );
                     },
                   ),
                 ],
               ),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                // InkWell(
-                //   onTap: () {
-                //     // Add your onTap functionality here
-                //   },
-                //   child: Column(
-                //     children: [Icon(Icons.fitness_center), Text('Workout')],
-                //   ),
-                // ),
                 InkWell(
                   onTap: () {
-                    // Add your onTap functionality here
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => RecipeListPage()),
+                      );
                   },
                   child: Column(
-                    children: [Icon(Icons.restaurant), Text('Nutrition')],
+                    children: const [Icon(Icons.restaurant), Text('Nutrition')],
                   ),
                 ),
                 InkWell(
@@ -93,7 +96,7 @@ class HomePage extends StatelessWidget {
                       );
                   },
                   child: Column(
-                    children: [Icon(Icons.card_membership), Text('Premium')],
+                    children: const [Icon(Icons.card_membership), Text('Premium')],
                   ),
                 ),
               ],
@@ -104,8 +107,8 @@ class HomePage extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 20.0),
                       child: Text(
                         'Workout',
                         style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
@@ -125,7 +128,7 @@ class HomePage extends StatelessWidget {
                               child: Container(
                                 width: 150,
                                 height: 100,
-                                margin: const EdgeInsets.only(right: 10), // Add spacing between items
+                                margin: const EdgeInsets.only(right: 10),
                                 color: Colors.grey[300],
                                 child: const Center(child: Text('Upper Body')),
                               ),
@@ -138,7 +141,7 @@ class HomePage extends StatelessWidget {
                               child: Container(
                                 width: 150,
                                 height: 100,
-                                margin: const EdgeInsets.only(right: 10), // Add spacing between items
+                                margin: const EdgeInsets.only(right: 10),
                                 color: Colors.grey[300],
                                 child: const Center(child: Text('Core')),
                               ),
@@ -151,7 +154,7 @@ class HomePage extends StatelessWidget {
                               child: Container(
                                 width: 150,
                                 height: 100,
-                                margin: const EdgeInsets.only(right: 10), // Add spacing between items
+                                margin: const EdgeInsets.only(right: 10),
                                 color: Colors.grey[300],
                                 child: const Center(child: Text('Lower Body')),
                               ),
@@ -160,60 +163,21 @@ class HomePage extends StatelessWidget {
                         ),
                       ),
                     ),
-
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.25,
-                      color: Colors.grey[200],
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      'Weekly Challenge',
-                                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text('Plank With Hip Twist'),
-                                  ),
-                                ],
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: GestureDetector(
-                                  onTap: () => _launchURL('https://example.com/challenge'),
-                                  child: Container(
-                                    height: MediaQuery.of(context).size.height * 0.15,
-                                    width: MediaQuery.of(context).size.width * 0.4,
-                                    color: Colors.grey[300],
-                                    child: Center(child: Text('Image Placeholder')),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+                    WeeklyChallenge(
+                      title: 'Weekly Challenge',
+                      description: 'Plank With Hip Twist',
+                      imageUrl: 'https://example.com/challenge',
+                      onTap: () => _launchURL('https://example.com/challenge'),
                     ),
-                    SizedBox(height: 20),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    const SizedBox(height: 20),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
                       child: Text(
                         'Articles & Tips',
                         style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                       ),
                     ),
-                    SizedBox(height: 10),
+                    const SizedBox(height: 10),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
@@ -223,7 +187,7 @@ class HomePage extends StatelessWidget {
                             width: 150,
                             height: 100,
                             color: Colors.grey[300],
-                            child: Center(child: Text('Supplement Guide')),
+                            child: const Center(child: Text('Supplement Guide')),
                           ),
                         ),
                         GestureDetector(
@@ -232,7 +196,7 @@ class HomePage extends StatelessWidget {
                             width: 150,
                             height: 100,
                             color: Colors.grey[300],
-                            child: Center(child: Text('Daily Routines')),
+                            child: const Center(child: Text('Daily Routines')),
                           ),
                         ),
                       ],
@@ -246,21 +210,19 @@ class HomePage extends StatelessWidget {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterFloat,
       floatingActionButton: SizedBox(
-        width: MediaQuery.of(context).size.width*0.5,
+        width: MediaQuery.of(context).size.width * 0.5,
         child: FloatingNavigationBar(
           onHomePressed: () => Navigator.pushAndRemoveUntil(
             context,
-            MaterialPageRoute(builder: (context) => HomePage()),
+            MaterialPageRoute(builder: (context) => const HomePage()),
             (route) => false,
           ),
-          // onChartPressed: () {
-          //   // Implement chart action here
-          // },
           onSettingsPressed: () => Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => const SettingPage()),
           ),
+        ),
       ),
-    ));
+    );
   }
 }
---
+++ lib/widget/weeklyChallenge.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class WeeklyChallenge extends StatelessWidget {
+  final String title;
+  final String description;
+  final String imageUrl;
+  final VoidCallback onTap;
+
+  const WeeklyChallenge({
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.onTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.25,
+      color: Colors.grey[200],
+      padding: const EdgeInsets.all(16.0),
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(description),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                      onTap: onTap,
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * 0.15,
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        color: Colors.grey[300],
+                        child: Center(child: Text('Image Placeholder')),
+                      ),
+                    ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
